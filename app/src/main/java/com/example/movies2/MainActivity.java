bf438ca30a906d19edaf5d13b3cd5d1b451e6491
sillva <isi2lva@gmail.com>
2020-08-19 10:34:02 +0300

    click on poster and reach end options

--- app/src/main/java/com/example/movies2/MainActivity.java
+++ app/src/main/java/com/example/movies2/MainActivity.java
@@ -6,6 +6,11 @@ import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 import android.util.Log;
+import android.view.TextureView;
+import android.view.View;
+import android.widget.CompoundButton;
+import android.widget.Switch;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import org.json.JSONObject;
@@ -18,9 +23,12 @@ import utils.NetworkUtils;
 
 public class MainActivity extends AppCompatActivity {
 
-    RecyclerView recyclerView;
-    MovieAdapter movieAdapter;
-    ArrayList<Movie> movies;
+    private RecyclerView recyclerView;
+    private MovieAdapter movieAdapter;
+    private ArrayList<Movie> movies;
+    private Switch switchSort;
+    private TextView textViewPopularity;
+    private TextView textViewTopRated;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -41,16 +49,73 @@ public class MainActivity extends AppCompatActivity {
 //            builder.append(movie.getTitle()).append("\n");
 //        }
 //        Log.d("494949", "onCreate: "+builder);
+        textViewPopularity = findViewById(R.id.textViewPopularity);
+        textViewTopRated = findViewById(R.id.textViewTopRated);
 
         recyclerView = findViewById(R.id.recyclerView);
         movieAdapter = new MovieAdapter(movies);
         recyclerView.setLayoutManager(new GridLayoutManager(this, 2));
-        JSONObject jsonObject = NetworkUtils.getJSONFromNetwork(NetworkUtils.POPULARITY,1);//получаем JSON объект в котором куча инфы о фильмах и сами фильмы
-        movies = JSONUtils.getMoviesFromJSON(jsonObject);//получаем эту кучу фильмов в виде массива
         movieAdapter.setMovies(movies);//приплетаем этот массив к РесВью чз адаптерг
         recyclerView.setAdapter(movieAdapter);//устанавливаем сопсна сам адаптерг на РесВью
 
 
+        //SWITCH:
+        switchSort = findViewById(R.id.switchSort);
+        switchSort.setChecked(true);
+        switchSort.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
+                setMethodOfSort(b);
+            }
+        });
+        switchSort.setChecked(false);
+        //SWITCH;
+        movieAdapter.setOnPosterClickListener(new MovieAdapter.OnPosterClickListener() {
+            @Override
+            public void onPosterClick(int position) {
+                Toast.makeText(MainActivity.this, "" + position, Toast.LENGTH_SHORT).show();
+            }
+        });
+        movieAdapter.setOnReachEndListener(new MovieAdapter.OnReachEndListener() {//при достижении конца списка
+            @Override
+            public void onReachEnd() {
+                Toast.makeText(MainActivity.this, "END", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        recyclerView.setAdapter(movieAdapter);//устанавливаем сопсна сам адаптерг на РесВью
+
+
+    }
+
+
+
+    public void setMethodOfSort(boolean isTopRated) { // этот метод для того чтобы, в зависимости от положения свитча, формировался ДЖСОН объект с характерной сортировкой
+        int methodOfSort;
+        if (isTopRated) {
+            methodOfSort = NetworkUtils.TOP_RATED;
+            textViewTopRated.setTextColor(getResources().getColor(R.color.colorAccent));
+            textViewPopularity.setTextColor(getResources().getColor(R.color.yellow));
+        } else {
+            methodOfSort = NetworkUtils.POPULARITY;
+
+            textViewPopularity.setTextColor(getResources().getColor(R.color.colorAccent));
+            textViewTopRated.setTextColor(getResources().getColor(R.color.yellow));
+        }
+        JSONObject jsonObject = NetworkUtils.getJSONFromNetwork(methodOfSort, 1);//получаем JSON объект в котором куча инфы о фильмах и сами фильмы
+        movies = JSONUtils.getMoviesFromJSON(jsonObject);//получаем эту кучу фильмов в виде массива
+        movieAdapter.setMovies(movies);//приплетаем этот массив к РесВью чз адаптерг
+    }
+
+    public void OnClickSetPopularity(View view) {
+        setMethodOfSort(true);
+        switchSort.setChecked(false);
+
+    }
+
+    public void OnClickSetTopRated(View view) {
+        setMethodOfSort(false);
+        switchSort.setChecked(true);
 
     }
 }
--- app/src/main/java/com/example/movies2/MovieAdapter.java
+++ app/src/main/java/com/example/movies2/MovieAdapter.java
@@ -1,6 +1,5 @@
 package com.example.movies2;
 
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -17,10 +16,27 @@ import data.Movie;
 
 public class MovieAdapter extends RecyclerView.Adapter<MovieAdapter.MovieViewHolder> {
 
-    public MovieAdapter() {
+    ArrayList<Movie> movies;
+    private OnPosterClickListener onPosterClickListener;//для щелчка по постер в РесВью
+    private OnReachEndListener onReachEndListener;//при достижении конца списка
+
+    public void setOnPosterClickListener(OnPosterClickListener onPosterClickListener) {//для щелчка по постер в РесВью
+        this.onPosterClickListener = onPosterClickListener;
     }
 
-    ArrayList<Movie> movies;
+
+    interface OnPosterClickListener {//для щелчка по постер в РесВью
+
+        void onPosterClick(int position);
+    }
+
+    public void setOnReachEndListener(OnReachEndListener onReachEndListener) {//при достижении конца списка
+        this.onReachEndListener = onReachEndListener;
+    }
+
+    interface OnReachEndListener {//при достижении конца списка
+        void onReachEnd();
+    }
 
     public ArrayList<Movie> getMovies() {
         movies = new ArrayList<>();
@@ -45,6 +61,9 @@ public class MovieAdapter extends RecyclerView.Adapter<MovieAdapter.MovieViewHol
 
     @Override
     public void onBindViewHolder(@NonNull MovieViewHolder holder, int position) {//не хватает конкретики по работе.
+        if( position == movies.size() - 1 && onReachEndListener != null){
+            onReachEndListener.onReachEnd();//при достижении конца списка. А точнее его конкретной позиции
+        }
         Movie movie = movies.get(position);//нужен для знания адреса постера
         ImageView imageView = holder.smallPoster;//элемент в РесВью,а ещё точнее - ИВ лайаута, который будет наполнять РесВью
         Picasso.get().load(movie.getPosterPath()).into(imageView);//в ИВ загружаем путь постера из Муви
@@ -63,12 +82,22 @@ public class MovieAdapter extends RecyclerView.Adapter<MovieAdapter.MovieViewHol
 
 
     class MovieViewHolder extends RecyclerView.ViewHolder {
-        private ImageView smallPoster;//movie_item layout сделали жe. Вот РесВью будет состоять из них
+        private ImageView smallPoster;//movie_item layout сделали жи. Вот РесВью будет состоять из них
 
         public MovieViewHolder(@NonNull View itemView) {
             super(itemView);
             smallPoster = itemView.findViewById(R.id.smallPosterForRecView);//itemView находит по пакету в лайаутах этот ИД?
+            itemView.setOnClickListener(new View.OnClickListener() {//для щелчка по постеру в РесВью
+                @Override
+                public void onClick(View view) {
+                    if (onPosterClickListener != null) {
+                        onPosterClickListener.onPosterClick(getAdapterPosition());//для щелчка по постеру в РесВью
+                    }
+                }
+            });
         }
+
+
     }
 
 }
